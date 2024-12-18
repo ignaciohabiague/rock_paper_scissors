9b551b3b1ad4f581cfb845cc56f81acb433879fa
Ignacio <ignacio.habiague1@gmail.com>
2024-12-18 15:16:09 -0300

    getHumanChoice function finished

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
-function getComputerChoice(){
-    let result = Math.random()
-    if(result<=0.3){
-        console.log("Rock!")
-    }
-    else{
-        if(result>0.3&&result<=0.6){
-            console.log("Paper!")
-        }
-        else{
-            console.log("Scissors!")
-        }
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        return "rock";
+    } else if (randomNum === 1) {
+        return "paper";
+    } else {
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let choice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        choice = prompt("Invalid choice. Please enter: rock, paper, or scissors").toLowerCase();
+    }
+    return choice;
+}
+
