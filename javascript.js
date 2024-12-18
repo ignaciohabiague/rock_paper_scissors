813544f591e22b30e6bb7d59d4268f19ac5e14b1
Ignacio <ignacio.habiague1@gmail.com>
2024-12-18 15:09:43 -0300

    getComputerChoice function finished

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello!")
+function getComputerChoice(){
+    let result = Math.random()
+    if(result<=0.3){
+        console.log("Rock!")
+    }
+    else{
+        if(result>0.3&&result<=0.6){
+            console.log("Paper!")
+        }
+        else{
+            console.log("Scissors!")
+        }
+    }
+}
+
+console.log(getComputerChoice())
